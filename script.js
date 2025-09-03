80387f0577d2acf0d3721c894abaa0aedc2bad7d
sowkat <sowkathossain.pro@gmail.com>
2025-09-04 00:26:34 +0600

    Call Button Functionality Added

--- script.js
+++ script.js
@@ -7,3 +7,37 @@ for(const heart of hearts){
     document.getElementById('heart-count').innerText = heartClicked;
     })
 }
+// call Button click functionality
+const callbtns = document.querySelectorAll('.call-btn');
+for(const callbtn of callbtns){
+    callbtn.addEventListener('click', function(){
+        let coinNumber= parseInt(document.getElementById('coin').innerText);
+    let newcoinNumber = coinNumber - 20;
+    const card = callbtn.parentNode.parentNode;
+    const serviceNumber = card.querySelector('.service-number').innerText;
+    const serviceName = card.querySelector('.service-name').innerText;
+    const serviceDept = card.querySelector('.service-dept').innerText;
+    if(coinNumber >= 20){
+        document.getElementById('coin').innerText = newcoinNumber;
+        
+        // Call history add
+        const history = document.getElementById('history');
+        const now = new Date();
+        let time = now.toLocaleTimeString();
+       const creatDiv = document.createElement('div');
+       creatDiv.innerHTML = `<div class="history bg-[#fafafa] p-3 mt-5">
+                    <div class="flex items-center justify-between">
+                        <div class="">
+                            <h2 class="font-semibold text-[16px]">${serviceName}</h2>
+                            <h3>${serviceNumber}</h3>
+                        </div>
+                        <div>${time}</div>
+                    </div>
+                </div>`
+                history.appendChild(creatDiv);
+        alert(`📞 Calling ${serviceName} ${serviceNumber} ...`);
+
+    }else{alert('❌ You have no Sufficient Balance, You need minimum 20 Coins to make a call')}
+
+    })
+}
